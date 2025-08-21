60fc29282fcf76d0b3778c185c09ac90c590bc15
abdoemadselim <abdoemadselim11@gmail.com>
2025-08-21 12:10:11 +0300

    frontend(auth): edit the layout of the auth pages

--- apps/frontend/src/app/(auth)/layout.tsx
+++ apps/frontend/src/app/(auth)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function Layout({
     children,
 }: Readonly<{
@@ -5,7 +8,21 @@ export default function Layout({
 }>) {
     return (
         <>
-            {children}
+            <div className="min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
+                <Link href="/">
+                    <Image
+                        src="/logo-lg.png"
+                        alt="مُختصِر"
+                        width="210"
+                        height="84"
+                        className="mx-auto"
+                    />
+                </Link>
+                <p className="text-center pb-8 text-muted-foreground">أول منتج عربي متكامل لإختصار الروابط</p>
+                <section className="flex">
+                    {children}
+                </section>
+            </div>
         </>
     );
 }
--- apps/frontend/src/app/(auth)/login/page.tsx
+++ apps/frontend/src/app/(auth)/login/page.tsx
@@ -1,24 +1,7 @@
 import LoginForm from "@/features/auth/components/login-form";
-import Image from "next/image";
-import Link from "next/link";
-
 
 export default function LoginPage() {
     return (
-        <div className="min-h-screen bg-zinc-50 px-4 py-16 md:py-32 dark:bg-transparent">
-            <Link href="/">
-                <Image
-                    src="/logo-lg.png"
-                    alt="مختصر"
-                    width="210"
-                    height="84"
-                    className="mx-auto"
-                />
-            </Link>
-            <p className="text-center pb-8 text-muted-foreground">أول منتج عربي متكامل لإختصار الروابط</p>
-            <section className="flex">
-                <LoginForm />
-            </section>
-        </div>
+        <LoginForm />
     )
 }
