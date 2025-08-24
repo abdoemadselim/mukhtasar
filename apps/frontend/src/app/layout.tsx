009cc3c4d39b57ffc3c711bd708d40e828da6541
abdoemadselim <abdoemadselim11@gmail.com>
2025-08-24 11:37:17 +0300

    feat(frontend): add vercel analytics

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/next"
 import "@/app/globals.css";
 import { cairo } from "@/app/fonts";
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       >
         {children}
         <SpeedInsights />
+        <Analytics />
       </body>
     </html>
   );
