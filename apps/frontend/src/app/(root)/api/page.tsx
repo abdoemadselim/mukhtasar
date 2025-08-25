b822559dd2cdf88c17b0db45c4f862afbbac3c24
abdoemadselim <abdoemadselim11@gmail.com>
2025-08-25 11:33:58 +0300

    refactor(security): updates for sake of security

--- apps/frontend/src/app/(root)/api/page.tsx
+++ apps/frontend/src/app/(root)/api/page.tsx
@@ -1,13 +1,13 @@
 "use client"
 
-import "swagger-ui-react/swagger-ui.css"
+// import "swagger-ui-react/swagger-ui.css"
 
-import SwaggerUI from "swagger-ui-react";
+// import SwaggerUI from "swagger-ui-react";
 
 export default function ApiDocsPage() {
     return (
         <div style={{ height: "100vh" }} dir="ltr">
-            <SwaggerUI url="/api-docs.json" />
+            {/* <SwaggerUI url="/api-docs.json" /> */}
         </div>
     )
 }
