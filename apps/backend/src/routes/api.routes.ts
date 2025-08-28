e64ac226ff1e36f662d988ec74aff8891b203807
abdoemadselim <abdoemadselim11@gmail.com>
2025-08-28 20:15:45 +0300

    refactor(doc): update the swagger doc ui and lang

--- apps/backend/src/routes/api.routes.ts
+++ apps/backend/src/routes/api.routes.ts
@@ -6,12 +6,25 @@ import swaggerApiDoc from "../../docs/api-doc.json" with {type: "json"}
 
 const router = Router()
 
+const customCss = `
+    .info {
+        text-align:right;
+    }
+    ul li {
+        padding-top: 15px;
+    }
+    ul li::marker {
+      content: "";
+    }
+
+`
 // Swagger UI setup with options    
 const swaggerOptions = {
     swaggerOptions: {
         showRequestDuration: true
     },
-    customSiteTitle: "مُختصِر | وثائق المبرمجين"
+    customSiteTitle: "مُختصِر | وثائق المبرمجين",
+    customCss: customCss,
 };
 
 // The API Doc
