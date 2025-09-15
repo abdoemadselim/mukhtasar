49e85091ff7004475d7822abc3013f470113bfa3
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-15 14:44:26 +0300

    feat(frontend): fix layout change in hero section

--- apps/frontend/src/components/layout/hero-section.tsx
+++ apps/frontend/src/components/layout/hero-section.tsx
@@ -22,15 +22,12 @@ export default function HeroSection() {
                     </SparklesText>
                 </div>
                 <AnimatedGradientText text="أول منتج عربي متكامل لإختصار الروابط" />
+                <div className="text-lg md:text-2xl lg:text-3xl md:max-w-[1000px] max-w-[530px] leading-relaxed px-4 pt-8">
+                    فهم أعمق لعملائك = استهداف أدق = نمو أسرع لشركتك!
+                    <p className="text-base md:text-md lg:text-xl text-muted-foreground leading-relaxed hidden sm:block">مع إحصاءات مُختصِر الدقيقة: نوع الجهاز، الدولة، المتصفح، مصدر الزيارة، وأكثر.</p>
 
-                <div className="pt-10 min-h-[100px] md:min-h-[120px] flex items-start justify-center">
-                    <p className="text-lg md:text-2xl lg:text-3xl md:max-w-[1000px] max-w-[530px] leading-relaxed px-4">
-                        فهم أعمق لعملائك = استهداف أدق = نمو أسرع لشركتك!
-                        <p className="text-base md:text-md lg:text-xl text-muted-foreground leading-relaxed hidden sm:block">مع إحصاءات مُختصِر الدقيقة: نوع الجهاز، الدولة، المتصفح، مصدر الزيارة، وأكثر.</p>
-
-                        <p className="pt-4"> كل روابطك تتكلم عنك</p>
-                        <p className="text-base md:text-md lg:text-xl text-muted-foreground leading-relaxed mb-4">مع روابط قصيرة معبرة تحمل نطاق موقعك</p>
-                    </p>
+                    <p className="pt-4"> كل روابطك تتكلم عنك</p>
+                    <p className="text-base md:text-md lg:text-xl text-muted-foreground leading-relaxed mb-4">مع روابط قصيرة معبرة تحمل نطاق موقعك</p>
                 </div>
             </header>
             <LandingUrlCreationForm />
