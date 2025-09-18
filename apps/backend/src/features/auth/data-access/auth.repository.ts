81edca97655fc139d9c248258665bad34a2b6f2d
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-18 11:00:27 +0300

    feat(auth): remove adding sample urls to new users

--- apps/backend/src/features/auth/data-access/auth.repository.ts
+++ apps/backend/src/features/auth/data-access/auth.repository.ts
@@ -8,7 +8,10 @@ import { query } from "#lib/db/db-connection.js";
 const authRepository = {
     async createUser({ name, email, password }: Omit<NewUserType, "password_confirmation">): Promise<UserType> {
         const result = await query(`
-            SELECT user_id as id, name, email FROM create_new_user_with_data($1, $2, $3)
+            INSERT INTO
+            users(name, email, password)
+            VALUES($1, $2, $3)
+            RETURNING id, name, email
         `, [email, password, name])
 
         return result.rows[0]
