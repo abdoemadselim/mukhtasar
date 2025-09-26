5abf88b41a62d240c79e6fe63b969645c5d6a477
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-26 16:56:13 +0300

    feat(email): update contact email template

--- apps/frontend/src/app/auth/(root)/login/page.tsx
+++ apps/frontend/src/app/auth/(root)/login/page.tsx
@@ -7,9 +7,9 @@ export default function LoginPage() {
     return (
         <div className="bg-card m-auto h-fit rounded-[calc(var(--radius)+.125rem)] border p-0.5 mx-4 shadow-md dark:[--color-muted:var(--color-zinc-900)]">
             <h1 className="mb-6 mt-4 text-xl text-center text-primary font-semibold">تسجيل الدخول</h1>
-            {/* <Separator />
+            <Separator />
             <OAuth />
-            <p className="text-center mt-4">أو</p> */}
+            <p className="text-center mt-4">أو</p>
             <LoginForm />
         </div>
     )
