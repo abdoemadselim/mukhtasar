970ea6c7267e41f2c732ff74acd17564823c898a
abdoemadselim <abdoemadselim11@gmail.com>
2025-09-26 15:17:03 +0300

    feat(frontend): comment social media login for now

--- apps/frontend/src/app/auth/(root)/signup/page.tsx
+++ apps/frontend/src/app/auth/(root)/signup/page.tsx
@@ -6,9 +6,9 @@ export default function SignUpPage() {
     return (
         <div className="bg-card m-auto h-fit rounded-[calc(var(--radius)+.125rem)] border p-0.5 mx-4 shadow-md dark:[--color-muted:var(--color-zinc-900)]">
             <h1 className="mb-6 mt-4 text-xl text-center text-primary font-semibold">أنشىء حسابك المجاني</h1>
-            <Separator />
+            {/* <Separator />
             <SocialMediaAuth />
-            <p className="text-center mt-4">أو</p>
+            <p className="text-center mt-4">أو</p> */}
             <SignUpForm />
         </div>
     )
